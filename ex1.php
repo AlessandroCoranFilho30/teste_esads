5808734ff89cee0e9ebd0ce35b4e47da67bad48e
Bruno H Pro <bruno.ferreira02@cps.sp.gov.br>
2026-04-29 15:37:03 -0300

    Arquivos da aula
    Exemplos criados durante a aula x.

---
+++ ex1.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP 101</title>
+</head>
+<body>
+    <?php
+            print date("d-m-y H:i:s") ."<br>";
+            echo("Desenvolvimento Web 2 na Fatec Araras")
+        ?>
+</body>
+</html>
+
---
+++ ex2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pr-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comentários</title>
+</head>
+<body>
+    <?php
+    //Comentário de Uma Linha
+    echo "Segunda-Feira!!!"
+    /*
+    echo "Terça-Ferira!!!"
+    */
+    ?>
+</body>
+</html>
